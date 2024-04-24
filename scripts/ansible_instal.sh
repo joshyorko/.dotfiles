1203957941d38269112c3cdcda7bf789a996e5f0
Josh Yorko <joshua.yorko@gainwelltechnologies.com>
2024-04-24 09:37:33 -0400

    Remove unused files and scripts, including jyorko_accessKeys.csv, ansible_instal.sh, and compress_and_move.sh

--- scripts/ansible_instal.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-sudo apt install software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
-
-
-bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
